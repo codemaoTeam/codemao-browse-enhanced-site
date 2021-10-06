629c04714bbbd8e1981b942c640bc4d32d0608c4
dagoyounger <dagoyounger@gmail.com>
2021-10-06 17:00:06 +0800

    improvement: 修改百度统计触发条件
    本地访问不会统计

--- public/js/bs.js
+++ public/js/bs.js
@@ -1,7 +1,9 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement('script');
-  hm.src = 'https://hm.baidu.com/hm.js?ad8afb886eb0bbf4043a602c77c1840e';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(hm, s);
-})();
+if ((location.hostname != 'localhost') & (location.hostname != '127.0.0.1')) {
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement('script');
+    hm.src = 'https://hm.baidu.com/hm.js?ad8afb886eb0bbf4043a602c77c1840e';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+}
