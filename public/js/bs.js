fb66e9c1ba6320a12d486127fd5048b56ef6d17b
dagoyounger <dagoyounger@gmail.com>
2021-10-06 16:09:33 +0800

    feat: 添加百度统计

---
+++ public/js/bs.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?ad8afb886eb0bbf4043a602c77c1840e';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(hm, s);
+})();
